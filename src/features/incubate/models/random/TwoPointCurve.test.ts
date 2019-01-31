1026d052c5806312f3414b71a009b074b367bdaf
Peter Leong <peter.leong@advancetech.io>
2019-01-31 11:06:48 -0800

    More TwoPointCurve tests

--- src/features/incubate/models/random/TwoPointCurve.test.ts
+++ src/features/incubate/models/random/TwoPointCurve.test.ts
@@ -4,9 +4,11 @@ import {TwoPointCurve} from "./TwoPointCurve";
 describe("TwoPointCurve tests", () => {
     describe("Given an upward TwoPointCurve", () => {
         const subject = TwoPointCurve({x: .1, y: .1}, {x: .3, y: .3});
+
         function check(x: number, expY: number) {
             assert.strictEqual(subject(x), expY);
         }
+
         describe("when I evaluate x", () => {
             it("should return an appropriate y", () => {
                 check(0, .1);
@@ -19,5 +21,40 @@ describe("TwoPointCurve tests", () => {
             });
         });
     });
-    // TODO: Test step curve, downward
+    describe("Given an downward TwoPointCurve", () => {
+        const subject = TwoPointCurve({x: .1, y: .3}, {x: .3, y: .1});
+
+        function check(x: number, expY: number) {
+            assert.strictEqual(subject(x), expY);
+        }
+
+        describe("when I evaluate x", () => {
+            it("should return an appropriate y", () => {
+                check(0, .3);
+                check(.1, .3);
+                check(.3, .1);
+                check(.4, .1);
+                check(.2, 0.19999999999999998);
+                check(.15, .25);
+                check(.25, .15);
+            });
+        });
+    });
+
+    describe("Given a step down TwoPointCurve", () => {
+        const subject = TwoPointCurve({x: .1, y: .3}, {x: .1, y: .1});
+
+        function check(x: number, expY: number) {
+            assert.strictEqual(subject(x), expY);
+        }
+
+        describe("when I evaluate x", () => {
+            it("should return an appropriate y", () => {
+                check(0, .3);
+                check(.1, .3);
+                check(.100000000001, .1);
+                check(.4, .1);
+            });
+        });
+    });
 });
